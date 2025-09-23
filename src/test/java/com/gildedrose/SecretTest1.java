d0b0cd4de2fc64b04deeed34dc1b5d9f04383a53
wyattcsutherland <wyattcsutherland@gmail.com>
2025-09-23 10:05:50 -0500

    rename

--- src/test/java/com/gildedrose/SecretTest1.java
+++ src/test/java/com/gildedrose/SecretTest1.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SecretTest {
+class SecretTest1 {
 
 //    @Test
 //    void agedBrieSellinDegradesByOneQualityIncreasesByTwo() {
